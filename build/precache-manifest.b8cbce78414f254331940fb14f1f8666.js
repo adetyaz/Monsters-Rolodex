28133053ce72b0b13e6cd697064a4d8d13598371
adetyaz <tayo.lasisi@yahoo.com>
2020-04-18 05:55:18 +0100

    initial commit

---
+++ build/precache-manifest.b8cbce78414f254331940fb14f1f8666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce12adb6edb056652379725f8e4580b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb2323982449ba46a314",
+    "url": "/monsters-rolodex/static/css/main.54246f9b.chunk.css"
+  },
+  {
+    "revision": "e6258cbbe686f7ab5cff",
+    "url": "/monsters-rolodex/static/js/2.9bf61b77.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.9bf61b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb2323982449ba46a314",
+    "url": "/monsters-rolodex/static/js/main.fcb253ee.chunk.js"
+  },
+  {
+    "revision": "c838ecf0a5dbd98e5c4f",
+    "url": "/monsters-rolodex/static/js/runtime-main.a3b126e5.js"
+  }
+]);
